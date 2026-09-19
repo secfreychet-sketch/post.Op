b46d323c41a09b3d5827e7946c6a54539ecf63b5
secfreychet-sketch <sec.freychet@gmail.com>
2026-09-19 18:39:16 +0200

    Rafraîchit le cache de l’onglet Objectifs

--- sw.js
+++ sw.js
@@ -2,14 +2,14 @@
    Met l'application en cache pour qu'elle fonctionne sans réseau.
    Incrémenter VERSION à chaque mise à jour du contenu. */
 
-const VERSION = "postop-v12-objectives";
+const VERSION = "postop-v13-objectives-criteria";
 const FILES = [
   "./",
   "./index.html",
   "./app.js?v=12",
   "./faq-data.js?v=12",
   "./faq-ui.js?v=12",
-  "./objectives-ui.js?v=12",
+  "./objectives-ui.js?v=13",
   "./manifest.json",
   "./icon-192.png",
   "./icon-512.png",
